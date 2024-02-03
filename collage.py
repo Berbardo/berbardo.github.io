0f1b1bb0532aff326877d5f3eae0923fcbe39280
Berbardo <bernardo.rcoutinho@gmail.com>
2024-02-03 00:41:51 -0300

    Otimiza imagens

--- collage.py
+++ collage.py
@@ -1,4 +1,5 @@
 import os
+from PIL import Image
 # from dotenv import load_dotenv
 from lastfmcollagegenerator.collage_generator import CollageGenerator
 
@@ -10,6 +11,8 @@ API_SECRET = os.environ.get("LAST_FM_API_SECRET")
 collage_generator = CollageGenerator(lastfm_api_key=API_KEY, lastfm_api_secret=API_SECRET)
 
 image = collage_generator.generate(entity="album", username="berbardo8", cols=4, rows=4, period="1month")
-image.save("img/collage.png", "png")
+
+image = image.resize((900,900), Image.LANCZOS)
+image.save("img/collage.webp", optimize=True)
 
 print("Successfully saved collage!")
