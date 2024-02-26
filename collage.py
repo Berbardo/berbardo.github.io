7e377fcc9d1c5c5a8e9f0bd6550e221dad411819
Berbardo <bernardo.rcoutinho@gmail.com>
2024-02-25 23:42:09 -0300

    fix collage

--- collage.py
+++ collage.py
@@ -8,11 +8,15 @@ from lastfmcollagegenerator.collage_generator import CollageGenerator
 API_KEY = os.environ.get("LAST_FM_API_KEY")
 API_SECRET = os.environ.get("LAST_FM_API_SECRET")
 
-collage_generator = CollageGenerator(lastfm_api_key=API_KEY, lastfm_api_secret=API_SECRET)
 
-image = collage_generator.generate(entity="album", username="berbardo8", cols=4, rows=4, period="1month")
+try:
+    collage_generator = CollageGenerator(lastfm_api_key=API_KEY, lastfm_api_secret=API_SECRET)
+    image = collage_generator.generate(entity="album", username="berbardo8", cols=4, rows=4, period="1month")
 
-image = image.resize((900,900), Image.LANCZOS)
-image.save("img/collage.webp", optimize=True)
+    image = image.resize((900,900), Image.LANCZOS)
+    image.save("img/collage.webp", optimize=True)
 
-print("Successfully saved collage!")
+    print("Successfully saved collage!")
+
+except:
+    print("Couldn't save collage, using old image!")
